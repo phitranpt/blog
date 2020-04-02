a1cd06885501d33c590114048f451f9e6b0da1c7
phitranpt <phi.tran.pt@gmail.com>
2020-04-01 21:42:46 -0500

    removed demo file

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import PostList from './PostList';
-import DemoFile from './DemoFile';
 
 const App = () => {
   return (
     <div className="ui container">
       <PostList />
-      <DemoFile />
     </div>
   )
 }
